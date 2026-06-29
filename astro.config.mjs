1d645e86607396aff3bee55a447785f695153082
Bernardo Santiago <bernardosantiago44@gmail.com>
2026-06-29 17:15:26 -0600

    Add Cloudflare Workers configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
@@ -9,6 +11,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
+  adapter: cloudflare(),
   integrations: [mdx()]
 });
