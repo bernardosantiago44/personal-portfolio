61551a83104df52808ca1f7080fede75cce2063f
Bernardo Santiago <bernardosantiago44@gmail.com>
2026-06-29 18:27:00 -0600

    Fixed image incorrect source

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 
-import cloudflare from "@astrojs/cloudflare";
-
-import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
   adapter: cloudflare(),
-  integrations: [mdx()]
+  integrations: [mdx()],
+  output: 'static'
 });
